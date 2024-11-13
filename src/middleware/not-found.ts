4917e631bf032b26c91fa650808fa38b17602c2d
Lauro <lauropimentel12@gmail.com>
2024-11-13 19:08:28 +0000

    first commit

---
+++ src/middleware/not-found.ts
@@ -0,0 +1,8 @@
+
+import { Response, Request, NextFunction } from "express";
+import { CustomError } from "../lib/custom-error";
+
+
+export function notFound(req: Request, res: Response, next: NextFunction) {
+  return next(new CustomError("Route not found", 404));
+}
